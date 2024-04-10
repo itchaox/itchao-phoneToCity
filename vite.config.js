dda2d5d4d60dfad1169186e34b3bbdd0ef809c16
itchaox <1462831573@qq.com>
2024-04-11 00:40:08 +0800

    feat: base 修改

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+/*
+ * @Version    : v1.00
+ * @Author     : itchaox
+ * @Date       : 2023-06-21 11:48
+ * @LastAuthor : itchaox
+ * @LastTime   : 2024-04-11 00:39
+ * @desc       :
+ */
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
-    host: true
+    host: true,
   },
   plugins: [
     vue(),
@@ -18,10 +27,11 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    }),],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
