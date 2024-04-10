62e9bb1261c458714c2201625a361f51db6716fe
itchaox <1462831573@qq.com>
2024-04-11 00:32:42 +0800

    feat: 所属列逻辑完善，loading 新增

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-26 15:10
  * @LastAuthor : itchaox
- * @LastTime   : 2024-04-09 00:07
+ * @LastTime   : 2024-04-11 00:30
  * @desc       : 
 -->
 <script setup>
@@ -19,23 +19,25 @@
 
   let dateFormatList = [
     {
-      name: '格式: 湖南长沙',
+      name: '格式: 广东深圳',
       value: '无',
     },
     {
-      name: '格式: 湖南-长沙',
+      name: '格式: 广东-深圳',
       value: '-',
     },
     {
-      name: '格式: 湖南_长沙',
+      name: '格式: 广东_深圳',
       value: '_',
     },
     {
-      name: '格式: 湖南/长沙',
+      name: '格式: 广东/深圳',
       value: '/',
     },
   ];
   let dateFormat = ref();
+
+  const loading = ref(false);
 
   onMounted(async () => {
     const table = await base.getActiveTable();
@@ -49,6 +51,7 @@
         type: 'error',
         message: '请选择手机号码列!',
       });
+
       return;
     }
 
@@ -60,36 +63,21 @@
       return;
     }
 
-    const table = await base.getActiveTable();
-    // 无手机号码所属地列表则创建, 有则提醒会覆盖数据
-    const fieldMetaList = await table.getFieldMetaList();
-    const hasBirthday = fieldMetaList.find((item) => item.name === '手机号码所属地');
-
-    if (!hasBirthday) {
-      await table.addField({ type: FieldType.Text, name: '手机号码所属地' });
-      generateBirthdayRow();
-    } else {
-      ElMessageBox.confirm('已存在(手机号码所属地列) ,后续操作会覆盖前面手机号码所属地列数据,请确认是否继续?', '警告', {
-        confirmButtonText: '确认',
-        cancelButtonText: '取消',
-        type: 'warning',
-      }).then(() => {
-        ElMessage({
-          type: 'success',
-          message: '开始生成数据...',
-        });
-        generateBirthdayRow();
-      });
-    }
+    generateBirthdayRow();
   }
+
+  // 所属地列
+  const areaId = ref();
 
   /**
    * @desc  : 生成手机号码所属地列
    */
   async function generateBirthdayRow() {
+    loading.value = true;
+
     const table = await base.getActiveTable();
     const recordList = await table.getRecordList();
-    const field = await table.getField('手机号码所属地'); // 选择某个多行文本字段
+    const field = await table.getField(areaId.value); // 选择某个多行文本字段
     const recordIds = await table.getRecordIdList(); // 获取所有记录 id
 
     let _list = [];
@@ -115,20 +103,40 @@
 
     await table.setRecords(_list);
 
+    loading.value = false;
     ElMessage({
-      message: '数据生成结束!',
+      message: '数据处理完成',
       type: 'success',
     });
   }
 </script>
 
 <template>
-  <div>
+  <div
+    v-loading="loading"
+    element-loading-text="加载中..."
+  >
     <div class="title">手机号码列</div>
     <div>
       <el-select
         v-model="fieldId"
         placeholder="请选择手机号码列"
+        size="large"
+      >
+        <el-option
+          v-for="item in fieldOptions"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+    </div>
+
+    <div class="title top">所属地列</div>
+    <div>
+      <el-select
+        v-model="areaId"
+        placeholder="请选择所属地列"
         size="large"
       >
         <el-option
@@ -160,7 +168,7 @@
       type="primary"
       class="btn"
       @click="confirm"
-      >确认</el-button
+      >运行</el-button
     >
   </div>
 </template>
