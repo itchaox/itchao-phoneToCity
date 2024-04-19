06a975d69bfd710890a25edb9e95019cb73ea301
itchaox <1462831573@qq.com>
2024-04-19 09:59:42 +0800

    feat: 处理 vite 打包报错问题

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-06-21 11:48
  * @LastAuthor : itchaox
- * @LastTime   : 2024-04-11 00:39
+ * @LastTime   : 2024-04-19 09:57
  * @desc       :
  */
 import { fileURLToPath, URL } from 'node:url';
@@ -13,10 +13,12 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  assetsInclude: ['**/*.dat'],
   server: {
     host: true,
   },
@@ -27,6 +29,10 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+    }),
+    topLevelAwait({
+      promiseExportName: '__tla',
+      promiseImportName: (i) => `__tla_${i}`,
     }),
   ],
   resolve: {
